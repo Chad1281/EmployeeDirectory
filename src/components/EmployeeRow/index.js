139128b8058fd4862c6f82ee46fd6a4d4c6eb1c3
Chad Cook <Charles.Cook28@gmail.com>
2021-03-14 16:30:02 -0500

    created index.js for HeaderRow

--- src/components/EmployeeRow/index.js
+++ src/components/EmployeeRow/index.js
@@ -2,17 +2,14 @@ import React from "react";
 import "./style.css";
 
 function EmployeeRow(props) {
-    return (
-        <div class="container">
-            <div class="row">
-                <div class="col">{props.name}</div>
-                <div class="col" >{props.phone}</div>
-                <div class="col">{props.email}</div>
-                <div class="col">{props.occupation}</div>
-                <div class="col">{props.location}</div> 
-            </div>
-            
-        </div>
+    return (        
+        <div class="row">
+            <div class="col">{props.name}</div>
+            <div class="col" >{props.phone}</div>
+            <div class="col">{props.email}</div>
+            <div class="col">{props.occupation}</div>
+            <div class="col">{props.location}</div> 
+        </div>        
     )
 }
 
---
+++ src/components/HeaderRow/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HeaderRow() {
+    return (
+        <div class="row">
+            <div class="col-sm-2">Name</div>
+            <div class="col-sm-2">Phone Number</div>
+            <div class="col-sm-2">Email</div>
+            <div class="col-sm-2">Occupation</div>
+            <div class="col-sm-2">Location</div>
+        </div>
+    )
+}
+
+export default HeaderRow;
