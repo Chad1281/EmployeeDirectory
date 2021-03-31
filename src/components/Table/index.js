a2d5ccd14047818172a110b32af25178fa4a4027
Chad Cook <Charles.Cook28@gmail.com>
2021-03-31 18:51:21 -0500

    installed bootstrap 3.7, changed some styling

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -8,49 +8,49 @@ function Table(props) {
         text: "Picture",
         sort: false,
         headerStyle: (colum, colIndex) => {
-            return { width: "150px", textAlign: "center" };
+            return { width: "150px", textAlign: "center", fontSize: "1.4em" };
           },
-        style: { alignItems: "center" }
+        style: { alignItems: "center"}
     }, {
         dataField: "name",
         text: "Name",
         sort: true,
         headerStyle: (colum, colIndex) => {
-            return { width: "15vw", textAlign: "center" };
+            return { width: "15vw", textAlign: "center", fontSize: "1.4em" };
           },
-        style: { paddingTop: "40px", fontSize: "1.2rem" }
+        style: { paddingTop: "50px", fontSize: "1.3em" }
     }, {
         dataField: "phone",
         text: "Phone Number",
         sort: false,
         headerStyle: (colum, colIndex) => {
-            return { width: "10vw", textAlign: "center" };
+            return { width: "10vw", textAlign: "center", fontSize: "1.4em" };
           },
-        style: { paddingTop: "40px", fontSize: "1.2rem"  }
+        style: { paddingTop: "50px", fontSize: "1.3em"  }
     }, {
         dataField: "email",
         text: "Email",
         sort: false,
         headerStyle: (colum, colIndex) => {
-            return { width: "20vw", textAlign: "center" };
+            return { width: "20vw", textAlign: "center", fontSize: "1.4em" };
           },
-        style: { paddingTop: "40px", fontSize: "1.2rem"  }
+        style: { paddingTop: "50px", fontSize: "1.3em"  }
     }, {
         dataField: "location",
         text: "Location",
         sort: true,
         headerStyle: (colum, colIndex) => {
-            return { width: "15vw", textAlign: "center" };
+            return { width: "15vw", textAlign: "center", fontSize: "1.4em" };
           },
-        style: { paddingTop: "40px", fontSize: "1.2rem"  }
+        style: { paddingTop: "50px", fontSize: "1.3em"  }
     }, {
         dataField: "country",
         text: "Country",
         sort: true,
         headerStyle: (colum, colIndex) => {
-            return { width: "10vw", textAlign: "center" };
+            return { width: "10vw", textAlign: "center", fontSize: "1.4em" };
           },
-        style: { paddingTop: "40px", fontSize: "1.2rem"  }
+        style: { paddingTop: "50px", fontSize: "1.3em"  }
     }];
 
     return <BootstrapTable 
